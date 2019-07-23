c66d74fd775ba2f33a7a5842186b12ffa8b07965
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2019-07-24 03:02:28 +0600

    Update run.py
    Ignore group messages.

--- run.py
+++ run.py
@@ -7,17 +7,16 @@ localtime = time.asctime( time.localtime(time.time()))
 with open('msg1.txt', encoding='utf8') as f:
 	txt = f.read().strip()
 
+
 final_text = txt + "\n\n\nTimestamp: " + localtime
+
 class CustomClient(Client):
 	def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
-		self.markAsDelivered(thread_id, message_object.uid)
-		self.markAsRead(thread_id)
 		if author_id != self.uid:
-			message_id = client.send(Message(text=final_text), thread_id=thread_id, thread_type=thread_type)
-		
-		
+			if thread_type != ThreadType.GROUP:
+				message_id = client.send(Message(text=final_text), thread_id=thread_id, thread_type=thread_type)
 
-client = CustomClient('email', 'password') # replace your username and password here
+client = CustomClient('username', 'password') # replace your username and password here
 session_cookies = client.getSession()
 client.getSession()
 client.listen()
