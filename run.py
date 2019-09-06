b1894ebc9892059aea3a9f00c752ca7ebf531b14
Md Sarwar Jahan Sabit <ssarwarjahan@gmail.com>
2019-09-06 17:14:32 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,26 @@
 from fbchat import Client
 from fbchat.models import *
 from random import randrange
-import time
+import time, threading
+from datetime import datetime, timedelta
+import sys
+import string
+from pygtail import Pygtail
+import pickle
 
-prev_authors = []
-bloc_authors = []
+
+prev_threads = []
+bloc_threads = []
+
+prebloc_threads = []
+
+received = []
+reply = []
+
+auto = '[Auto Reply by bot! 🤖 ] \n '
+
+
+
 
 with open('msg1.txt', encoding='utf8') as my_file1:
 	txt1 = my_file1.read().strip()
@@ -12,28 +28,132 @@ with open('msg1.txt', encoding='utf8') as my_file1:
 with open('msg2.txt', encoding='utf8') as my_file2:
 	txt2 = my_file2.read().strip()
 
-with open('blacklist.txt', encoding='utf') as my_file3:
+with open('blacklist.txt', encoding='utf8') as my_file3:
 	for line in my_file3:
-		bloc_authors.append(str(int(line)))
+		prebloc_threads.append(line.strip())
+
+with open('received.txt', encoding='utf8') as my_file4:
+	for line in my_file4:
+		received.append(line.strip())
+
+with open('reply.txt', encoding='utf8') as my_file5:
+	for line in my_file5:
+		reply.append(line.strip())
+
+
+
+
+def reload1():
+	for line in Pygtail("received.txt"):
+		received.append(line.strip())
+	for line in Pygtail("reply.txt"):
+	    reply.append(line.strip())
+	for line in Pygtail("blacklist.txt"):
+	 	prebloc_threads.append(line.strip())
+
+print(prebloc_threads)
+
+def reload2():
+	with open('msg1.txt', encoding='utf8') as my_file1:
+	 	txt1 = my_file1.read().strip()
+	with open('msg2.txt', encoding='utf8') as my_file2:
+		txt2 = my_file2.read().strip()
+	 	        
+
 
 class CustomClient(Client):
 	def onMessage(self, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
-		localtime = time.asctime(time.localtime(time.time()))
+		reload1()
+		reload2()
+		localtime = str(datetime.now() + timedelta(hours=6))
 		time.sleep(randrange(4,10))
-		primary_text = txt1 + "\nTimestamp: " + localtime
-		secondary_text = txt2 + "\nTimestamp: " + localtime
+		thread = self.fetchThreadInfo(thread_id)[thread_id]
+		primary_text = auto + '\n' + thread.name + ", " + txt1 + "\nTimestamp: " + localtime
+		secondary_text = auto + '\n' + thread.name + ", " +  txt2 + "\nTimestamp: " + localtime
+		
+		index = 100000
+		
+		signal = 1
+		text = message_object.text
+		
+		try:
+			matching = text.lower()
+			matching = matching.translate(str.maketrans('', '', string.punctuation))
+		except:
+			matching = None
+
+		'''if author_id == self.uid:
+			if thread_type != ThreadType.Group:
+				if text is 'bot100, stop replying':
+					self.send(Message(text= auto + '\n okay, stopping reply to ' + thread.name), thread_id=thread_id, thread_type=thread_type)
+					bloc_threads.append(thread_id)
+				if text is 'bot100, restart replying':
+					message_id = client.send(Message(text=auto + ' \n okay, restarting reply to ' + thread.name), thread_id=thread_id, thread_type=thread_type)
+					bloc_threads.remove(thread_id)
+				if text is 'bot100, add received':
+					message_id = client.send(Message(text=auto + ' \n okay, waiting for received data... (12 seconds remaining)'), thread_id=thread_id, thread_type=thread_type)
+					time.sleep(12)
+					if text != 'bot100, add received':
+						received.append(text + '\n')
+						with open("received.txt", "a") as my_file4:
+							my_file4.write(text + '\n')
+						message_id = client.send(Message(text=auto + ' \n added received data in array and file.'), thread_id=thread_id, thread_type=thread_type)
+				if text is 'bot100, add reply':
+					message_id = client.send(Message(text=auto + ' \n okay, waiting for reply data... (12 seconds remaining)'), thread_id=thread_id, thread_type=thread_type)
+					time.sleep(12)
+					if text != 'bot100, add reply':
+						reply.append(text + '\n')
+						with open("reply.txt", "a") as my_file5:
+							my_file5.write(text + '\n')
+						message_id = client.send(Message(text=auto + ' \n added reply data in array and file.'), thread_id=thread_id, thread_type=thread_type)
+				if text is 'bot100, shutdown':
+					message_id = client.send(Message(text=auto + ' \n Shutting Down...'), thread_id=thread_id, thread_type=thread_type)
+					time.sleep(3)
+					sys.exit()'''
+					
 		if author_id != self.uid:
 			if thread_type != ThreadType.GROUP:
-				if author_id in bloc_authors:
-					None
-				elif author_id in prev_authors:
-					message_id = client.send(Message(text=secondary_text), thread_id=thread_id, thread_type=thread_type)
-					bloc_authors.append(author_id)
-				else:
+				
+				if matching != None:
+					for replies in received:
+						if matching in replies:
+							index = received.index(replies)
+							print(type(index))
+							msg = auto + reply[index]
+							message_id = client.send(Message(text=msg), thread_id=thread_id, thread_type=thread_type)
+							print(type(message_id))
+							break
+						else:
+							signal = 0
+							
+				if thread_id not in prebloc_threads:
+					if thread_id not in bloc_threads:
+						if thread_id in prev_threads and matching != None and index == 100000:
+							message_id = client.send(Message(text=secondary_text), thread_id=thread_id, thread_type=thread_type)
+							bloc_threads.append(thread_id)
+			
+			
+				
+					
+					
+				if thread_id not in prev_threads and thread_id not in bloc_threads and thread_id not in prebloc_threads and matching != None and index == 100000:
 					message_id = client.send(Message(text=primary_text), thread_id=thread_id, thread_type=thread_type)
-					prev_authors.append(author_id)
+					prev_threads.append(thread_id)
+				else:
+					None
 
-client = CustomClient('email', 'password') # replace your username and password here
-session_cookies = client.getSession()
-client.getSession()
+try:
+	print('Checking for cookies...')
+	session_cookies = pickle.load(open('cookies.p','rb'))
+	client = CustomClient("cookie", "cookie", session_cookies = session_cookies) # replace your username and password here
+	print('Cookies found!')
+except:
+	print('Cookies not found. Please enter credentials.')
+	client = CustomClient(input('Email: '), input('Password: ')) # replace your username and password here
+	session = client.getSession()
+	pickle.dump(session, open('cookies.p','wb'))
+	print('Cookies saved to cookies.p file.')
+
+
+print('Starting program...')
 client.listen()
